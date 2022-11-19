ab3174ef93fa3c66e016da387e5bc2f272f464ef
rwony <dev.rwlee@gmail.com>
2022-11-19 22:46:27 +0900

    🚧 Add level 0 coding test

---
+++ level-0/잘라서-배열로-저장하기.js
@@ -0,0 +1,13 @@
+// https://github.com/rwony/programmers
+// 120913
+
+function solution(my_str, n) {
+  let answer = []
+
+  for (let i = 0; i < my_str.length / n; i++) {
+    answer = [...answer, my_str.slice(i * n, i * n + n)]
+  }
+  return answer
+}
+
+solution('abcdef123', 3)
